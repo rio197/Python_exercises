76d7cb204dacc620b3a6782c4008950606c31613
rio197 <thomasvictorio@yahoo.com>
2021-12-12 20:49:02 -0500

    First commit
    First commit

---
+++ How_to_return_parent_directory.py
@@ -0,0 +1,12 @@
+import os
+def parent_directory():
+  # Create a relative path to the parent
+  # of the current working directory
+
+  relative_parent = os.path.join("..", ".")
+  print(relative_parent)
+
+  # Return the absolute path of the parent directory
+  return os.path.abspath(relative_parent)
+
+print(parent_directory())
