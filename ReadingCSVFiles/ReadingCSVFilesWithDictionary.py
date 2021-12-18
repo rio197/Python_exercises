89688ef9071e95049f9410559aa1620ef618ab2e
rio197 <thomasvictorio@yahoo.com>
2021-12-18 18:08:02 -0500

    Replaced with correct content
    Replaced with correct content

--- ReadingCSVFiles/ReadingCSVFilesWithDictionary.py
+++ ReadingCSVFiles/ReadingCSVFilesWithDictionary.py
@@ -1,10 +1,6 @@
 import csv
 
-users = [{"name":"SolMansi", "username": "solm", "department":  "IT infrastructure"},{"name":"Lio Nelson", "username": "lion", "department":  "User Experience Research"},{"name":"Charlie Grey", "username": "greyc", "department":  "Development"}]
-
-keys = ["name", "username", "department"]
-
-with open('by_department.csv', 'w', newline='', encoding='utf-8') as by_department:
-    writer = csv.DictWriter(by_department, fieldnames=keys)
-    writer.writeheader()
-    writer.writerows(users)
+with open('software.csv') as software:
+    reader = csv.DictReader(software)
+    for row in reader:
+        print("{} has {} users".format(row["name"], row["users"]))
