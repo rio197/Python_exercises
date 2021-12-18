c18673cdc4644dfa0c07395d10a35128632c97aa
rio197 <thomasvictorio@yahoo.com>
2021-12-18 17:52:57 -0500

    Add newline & encoding parameters
    Add newline & encoding parameters

--- WriteCSVFiles/WriteCSVfiles.py
+++ WriteCSVFiles/WriteCSVfiles.py
@@ -2,6 +2,6 @@ import csv
 
 hosts = [["workstation.local", "192.168.25.46"], ["webserver.cloud","10.2.5.6"]]
 
-with open('hosts.csv', 'w') as hosts_csv:
+with open('hosts.csv', 'w', newline='', encoding='utf-8') as hosts_csv:
     writer = csv.writer(hosts_csv)
     writer.writerows(hosts)
