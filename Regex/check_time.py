4d759f930f1d5e35bada42604d2414ffd4595e61
rio197 <thomasvictorio@yahoo.com>
2022-02-06 22:07:09 -0500

    Separate the import section
    Separate the import section

--- Regex/check_time.py
+++ Regex/check_time.py
@@ -2,6 +2,7 @@
 # followed by a colon, then minutes between 00 and 59, then an optional space, and then AM or PM, in upper or lower case. 
 
 import re
+
 def check_time(text):
   pattern = r"(([0-9]|1[0-2]):([0-5][0-9])) ?am|AM|pm|PM"
   result = re.search(pattern, text)
