f72f3e365d91a21f507e1981730d86b5654884e4
rio197 <thomasvictorio@yahoo.com>
2022-02-17 17:23:16 -0500

    Third commit
    Third commit

--- How_to_delete_old_files.py
+++ How_to_delete_old_files.py
@@ -21,8 +21,11 @@ def main():
   if os.path.exists(path):
     # open the output file in Append mode
     with open ("list_old_files.output.txt","a") as file:
-      # use glob
-      
+      # use the glob module
+      ii = 1
+      # define the path to search in
+      for filepath in glob.iglob(r"\\somedrive\somefolder",recursive=True):
+        if ii = 100
   
 def get_file_or_folder_age(path):
   # time will be in seconds
